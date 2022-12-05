809d405ad883331b8e13db43a5f14470363c0a7f
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-12-05 13:13:28 +0530

    Create b_install.sh

---
+++ core/b_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pkg update && apt update && pkg upgrade && apt upgrade
+
+clear
+
+# Edit bash.bashrc File
+cd
+cd .. && cd usr && cd etc
+rm -rf motd
+
+echo
+
+cd 
+
+cd $HOME/TEAMSTARWORLD/core
+
+cat bash_bashrc.txt >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
